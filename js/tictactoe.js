ff0be7a2631b78e9c674f6b54f45683fa63e320b
johlmike <johlmike@gmail.com>
2016-06-21 14:49:18 +0800

    Create the playBoard Object
    Create playBoard Object to store all info about game, and a block object
    for all nine blocks.

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,3 +1,5 @@
+/* jshint esversion:6 */
+
 var ticTacToe = (function() {
     var exports = {};
 
@@ -29,9 +31,12 @@ var ticTacToe = (function() {
         $('#finish').remove();
         $('body').prepend($boardDiv);
 
-
-        //純粹測試用，請記得刪除
-        setTimeout(exports.showFinish, 1000);
+        $(".boxes").empty();
+        for( let i = 7 ; i > 0 ; i -= 3 ){
+            $(".boxes").append('<li class="box" id="b_' + i + '"></li>');
+            $(".boxes").append('<li class="box" id="b_' + (i+1) + '"></li>');
+            $(".boxes").append('<li class="box" id="b_' + (i+2) + '"></li>');
+        }
     };
 
     exports.showFinish = function(winner) {
@@ -43,7 +48,99 @@ var ticTacToe = (function() {
         });
     };
 
+    class block {
+        constructor(status = 0){ // 0 = nothing
+            this.status = status;
+        }
+        setO() {
+            this.status = 1; // 1 = o
+        }
+        setX() {
+            this.status = -1; // -1 = x
+        }
+    }
+
+    class playBoard {
+        constructor(oName, xName = "NPC"){
+            this.oName = oName;
+            this.xName = xName;
+            this.gameStatus = "not_finish";
+            this.turn = "o";
+
+            //Same as keyboard's number pad
+            this.b_1 = new block();
+            this.b_2 = new block();
+            this.b_3 = new block();
+            this.b_4 = new block();
+            this.b_5 = new block();
+            this.b_6 = new block();
+            this.b_7 = new block();
+            this.b_8 = new block();
+            this.b_9 = new block();
+        }
+        checkGameStatus(){
+            var blockArray = [this.b_1, this.b_2, this.b_3, this.b_4, this.b_5, this.b_6, this.b_7, this.b_8, this.b_9];
+            //check horizontal
+            for( let i = 0 ; i < 7 ; i += 3 ){
+                if( (blockArray[i].status + blockArray[i+1].status + blockArray[i+2].status) === 3 ){
+                    //o win the game
+                    this.gameStatus = "o_win";
+                    return console.log("o win");
+                } else if ( (blockArray[i].status + blockArray[i+1].status + blockArray[i+2].status) === -3 ){
+                    //x win the game
+                    this.gameStatus = "x_win";
+                    return console.log("x win");
+                }
+            }
+            //check vertical
+            for( let i = 0 ; i < 3 ; i += 1 ){
+                if( (blockArray[i].status + blockArray[i+3].status + blockArray[i+6].status) === 3 ){
+                    //o win the game
+                    this.gameStatus = "o_win";
+                    return console.log("o win");
+                } else if ( (blockArray[i].status + blockArray[i+3].status + blockArray[i+6].status) === -3 ){
+                    //x win the game
+                    this.gameStatus = "x_win";
+                    return console.log("x win");
+                }
+            }
+            //check oblique
+            if( (blockArray[0].status + blockArray[4].status + blockArray[8].status) === 3 ){
+                //o win the game
+                this.gameStatus = "o_win";
+                return console.log("o win");
+            } else if ( (blockArray[0].status + blockArray[4].status + blockArray[8].status) === -3 ){
+                //x win the game
+                this.gameStatus = "x_win";
+                return console.log("x win");
+            }
+
+            if ( (blockArray[2].status + blockArray[4].status + blockArray[6].status) === 3 ){
+                //x win the game
+                this.gameStatus = "o_win";
+                return console.log("o win");
+            } else if ( (blockArray[2].status + blockArray[4].status + blockArray[6].status) === -3 ){
+                //x win the game
+                this.gameStatus = "x_win";
+                return console.log("x win");
+            }
+            //check if all block are not 0
+            for( let i = 0 ; i < 9 ; i++){
+                var check = 0;
+                if( blockArray[i].status === 0){
+                    this.gameStatus = "not_finish";
+                    return console.log("not finish");
+                } else {
+                    check++;
+                }
+                if( check === 9 ){
+                    this.gameStatus = "draw";
+                    return console.log("draw");
+                }
+            }
+        }
+    }
+    exports.board = new playBoard("player1", "player2");
 
     return exports;
-
 })();
